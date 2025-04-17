1ac6939dcbe908366482af33fb3a0dc34809ab47
Kairnex <codexkairnex@gmail.com>
2025-04-17 20:22:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,140 +1,147 @@
 import re
-from os import environ,getenv
-from Script import script 
+import os
+from os import getenv
+
+from Script import script
 
 id_pattern = re.compile(r'^.\d+$')
+
 def is_enabled(value, default):
-    if value.lower() in ["true", "yes", "1", "enable", "y"]:
+    try:
+        val = value.lower()
+    except AttributeError:
+        return default
+    if val in ["true", "yes", "1", "enable", "y"]:
         return True
-    elif value.lower() in ["false", "no", "0", "disable", "n"]:
+    elif val in ["false", "no", "0", "disable", "n"]:
         return False
     else:
         return default
 
 # Bot information
-SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
-BOT_TOKEN = environ.get('BOT_TOKEN', "")
+SESSION     = os.environ.get('SESSION', 'Media_search')
+API_ID      = int(os.environ.get('API_ID', '9696783'))
+API_HASH    = os.environ.get('API_HASH', '3e74a9830493e9261410a947428dbb34')
+BOT_TOKEN   = os.environ.get('BOT_TOKEN', "7770072777:AAHcXrIxHfBjKVvlKs1x7-r5qeBjBGDO5r4")
 
 # Bot settings
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
+CACHE_TIME         = int(os.environ.get('CACHE_TIME', '300'))
+USE_CAPTION_FILTER = is_enabled(os.environ.get('USE_CAPTION_FILTER', 'True'), True)
 
-PICS = (environ.get('PICS', 'https://telegra.ph/file/65fe86fc02a73f6fcf0ce.jpg https://telegra.ph/file/6fa70325813885809a64a.jpg https://telegra.ph/file/e06afc1e7abbcd8d4213a.jpg https://telegra.ph/file/3f4040b320d9b7840200a.jpg https://telegra.ph/file/3950fad740fb8ea894df7.jpg')).split() #SAMPLE PIC
-NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
-MELCOW_VID = environ.get("MELCOW_VID", "https://telegra.ph/file/85d361ab4cb6511006022.mp4")
-SPELL_IMG = environ.get("SPELL_IMG", "https://telegra.ph/file/86b7b7e2aa7e38f328902.jpg")
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://telegra.ph/file/734170f40b8169830d821.jpg'))
-CODE = (environ.get('CODE', 'https://telegra.ph/file/72f425007b22d28bd935e.jpg'))
+PICS         = os.environ.get('PICS', 'https://files.catbox.moe/q9a07c.jpg').split()
+NOR_IMG      = os.environ.get('NOR_IMG', "https://files.catbox.moe/q9a07c.jpg")
+MELCOW_VID   = os.environ.get('MELCOW_VID', "https://files.catbox.moe/q9a07c.jpg")
+SPELL_IMG    = os.environ.get('SPELL_IMG', "https://files.catbox.moe/q9a07c.jpg")
+SUBSCRIPTION = os.environ.get('SUBSCRIPTION', 'https://files.catbox.moe/q9a07c.jpg')
+CODE         = os.environ.get('CODE', 'https://files.catbox.moe/q9a07c.jpg')
 
-#stream link shortner
-STREAM_SITE = (environ.get('STREAM_SITE', 'ziplinker.net'))
-STREAM_API = (environ.get('STREAM_API', 'ae0832f39e24094a0661626a792e6a2b8880e310'))
-STREAMHTO = (environ.get('STREAMHTO', 'https://t.me/HowToOpenLinkHP/69'))
+# Stream link shortener (you can now safely ignore these if unused)
+STREAM_SITE = os.environ.get('STREAM_SITE', '')
+STREAM_API  = os.environ.get('STREAM_API', '')
+STREAMHTO   = os.environ.get('STREAMHTO', '')
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '').split()] #Channel id for auto indexing ( make sure bot is admin )
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
-AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-PREMIUM_USER = [int(user) if id_pattern.search(user) else user for user in environ.get('PREMIUM_USER', '').split()]
-auth_channel = environ.get('AUTH_CHANNEL', '') #Channel / Group Id for force sub ( make sure bot is admin )
-auth_grp = environ.get('AUTH_GROUP')
-AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
-AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
-support_chat_id = environ.get('SUPPORT_CHAT_ID', '') # support group id ( make sure bot is admin )
-reqst_channel = environ.get('REQST_CHANNEL_ID', '') # request channel id ( make sure bot is admin )
-REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
+ADMINS        = [int(x) if id_pattern.search(x) else x for x in os.environ.get('ADMINS', '').split()]
+CHANNELS      = [int(x) if id_pattern.search(x) else x for x in os.environ.get('CHANNELS', '').split()]
+auth_users    = [int(x) if id_pattern.search(x) else x for x in os.environ.get('AUTH_USERS', '').split()]
+AUTH_USERS    = auth_users + ADMINS if auth_users else []
+PREMIUM_USER  = [int(x) if id_pattern.search(x) else x for x in os.environ.get('PREMIUM_USER', '').split()]
+
+auth_channel  = os.environ.get('AUTH_CHANNEL', '')
+AUTH_CHANNEL  = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
+auth_grp      = os.environ.get('AUTH_GROUP', '')
+AUTH_GROUPS   = [int(x) for x in auth_grp.split()] if auth_grp else []
+
+support_chat_id = os.environ.get('SUPPORT_CHAT_ID', '')
 SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(support_chat_id) else None
-NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False)) # True if you want no results messages in Log Channel
+
+reqst_channel = os.environ.get('REQST_CHANNEL_ID', '')
+REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
+
+NO_RESULTS_MSG = is_enabled(os.environ.get("NO_RESULTS_MSG", "False"), False)
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "")
-DATABASE_NAME = environ.get('DATABASE_NAME', "")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_URI    = os.environ.get('DATABASE_URI', "mongodb+srv://codexkairnex:gm6xSxXfRkusMIug@cluster0.bplk1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME   = os.environ.get('DATABASE_NAME', "mbot")
+COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Telegram_files')
 
-# Verify
-VERIFY = bool(environ.get('VERIFY', False)) # Verification On ( True ) / Off ( False )
-HOWTOVERIFY = environ.get('HOWTOVERIFY', url='https://t.me/') # How to open tutorial link for verification
+# Verification
+VERIFY      = is_enabled(os.environ.get('VERIFY', 'False'), False)
+HOWTOVERIFY = os.environ.get('HOWTOVERIFY', 'https://t.me/kairnex')
 
-# Others
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'instantearn.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', '1502a197c85d59929d50f1cba1d5e6f967d1e962')
-IS_SHORTLINK = bool(environ.get('IS_SHORTLINK', False))
-DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
-MAX_B_TN = environ.get("MAX_B_TN", "5")
-MAX_BTN = is_enabled((environ.get('MAX_BTN', "True")), True)
-PORT = environ.get("PORT", "8080")
-GRP_LNK = environ.get('GRP_LNK', 'https://t.me/')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/')
-TUTORIAL = environ.get('TUTORIAL', 'https://t.me/') # Tutorial video link for opening shortlink website 
-IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
-MSG_ALRT = environ.get('MSG_ALRT', 'ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : ʜᴘ')
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '')) #Log channel id ( make sure bot is admin )
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/') #Support group link ( make sure bot is admin )
-P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
-IMDB = is_enabled((environ.get('IMDB', "False")), False)
-AUTO_FFILTER = is_enabled((environ.get('AUTO_FFILTER', "True")), True)
-AUTO_DELETE = is_enabled((environ.get('AUTO_DELETE', "True")), True)
-SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), True)
-CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
-BATCH_FILE_CAPTION = environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
-IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", f"{script.IMDB_TEMPLATE_TXT}")
-LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
-SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
-MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
-INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
-FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
-MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
-PROTECT_CONTENT = is_enabled((environ.get('PROTECT_CONTENT', "True")), True)
-PUBLIC_FILE_STORE = is_enabled((environ.get('PUBLIC_FILE_STORE', "True")), True)
+# Miscellaneous
+DELETE_CHANNELS = [int(x) if id_pattern.search(x) else x for x in os.environ.get('DELETE_CHANNELS', '0').split()]
+MAX_B_TN        = int(os.environ.get("MAX_B_TN", '5'))
+MAX_BTN         = is_enabled(os.environ.get('MAX_BTN', "True"), True)
+PORT            = os.environ.get("PORT", "8080")
+GRP_LNK         = os.environ.get('GRP_LNK', 'https://t.me/kairnex')
+CHNL_LNK        = os.environ.get('CHNL_LNK', 'https://t.me/kairnex')
+TUTORIAL        = os.environ.get('TUTORIAL', 'https://t.me/kairnex')
+IS_TUTORIAL     = is_enabled(os.environ.get('IS_TUTORIAL', 'True'), True)
+MSG_ALRT        = os.environ.get('MSG_ALRT', 'ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ : ᴋᴀɪʀɴᴇx')
+LOG_CHANNEL     = int(os.environ.get('LOG_CHANNEL', '-1002083345153'))
+SUPPORT_CHAT    = os.environ.get('SUPPORT_CHAT', 'https://t.me/kairnex')
+
+P_TTI_SHOW_OFF     = is_enabled(os.environ.get('P_TTI_SHOW_OFF', "False"), False)
+IMDB               = is_enabled(os.environ.get('IMDB', "False"), False)
+AUTO_FFILTER       = is_enabled(os.environ.get('AUTO_FFILTER', "True"), True)
+AUTO_DELETE        = is_enabled(os.environ.get('AUTO_DELETE', "True"), True)
+SINGLE_BUTTON      = is_enabled(os.environ.get('SINGLE_BUTTON', "True"), True)
+CUSTOM_FILE_CAPTION= os.environ.get("CUSTOM_FILE_CAPTION", script.CAPTION)
+BATCH_FILE_CAPTION = os.environ.get("BATCH_FILE_CAPTION", CUSTOM_FILE_CAPTION)
+IMDB_TEMPLATE      = os.environ.get("IMDB_TEMPLATE", script.IMDB_TEMPLATE_TXT)
+LONG_IMDB_DESCRIPTION = is_enabled(os.environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
+SPELL_CHECK_REPLY     = is_enabled(os.environ.get("SPELL_CHECK_REPLY", "True"), True)
+MAX_LIST_ELM          = int(os.environ.get("MAX_LIST_ELM", 0)) if os.environ.get("MAX_LIST_ELM") else None
+INDEX_REQ_CHANNEL     = int(os.environ.get('INDEX_REQ_CHANNEL', str(LOG_CHANNEL)))
+FILE_STORE_CHANNEL    = [int(x) for x in os.environ.get('FILE_STORE_CHANNEL', '').split() if x]
+MELCOW_NEW_USERS      = is_enabled(os.environ.get('MELCOW_NEW_USERS', "True"), True)
+PROTECT_CONTENT       = is_enabled(os.environ.get('PROTECT_CONTENT', "True"), True)
+PUBLIC_FILE_STORE     = is_enabled(os.environ.get('PUBLIC_FILE_STORE', "True"), True)
 
 LANGUAGES = ["malayalam", "", "tamil", "", "english", "", "hindi", "", "telugu", "", "kannada", "", "gujarati", "", "marathi", "", "punjabi", ""]
-
-SEASONS = ["season 1" , "season 2" , "season 3" , "season 4", "season 5" , "season 6" , "season 7" , "season 8" , "season 9" , "season 10"]
-
+SEASONS   = [f"season {i}" for i in range(1, 11)]
 QUALITIES = ["360P", "", "480P", "", "720P", "", "1080P", "", "1440P", "", "2160P", ""]
 
-# Online Stream and Download
-NO_PORT = bool(environ.get('NO_PORT', False))
-APP_NAME = None
-if 'DYNO' in environ:
-    ON_HEROKU = True
-    APP_NAME = environ.get('APP_NAME')
-else:
-    ON_HEROKU = False
-BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-URL = "https://{}/".format(FQDN) if ON_HEROKU or NO_PORT else \
-    "https://{}/".format(FQDN, PORT)
-SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
-WORKERS = int(environ.get('WORKERS', '4'))
-SESSION_NAME = str(environ.get('SESSION_NAME', 'chikubot'))
-MULTI_CLIENT = False
-name = str(environ.get('name', 'LazyPrincess'))
-PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-if 'DYNO' in environ:
-    ON_HEROKU = True
-    APP_NAME = str(getenv('APP_NAME'))
+# Online stream & download
+NO_PORT     = is_enabled(os.environ.get('NO_PORT', 'False'), False)
+ON_HEROKU   = 'DYNO' in os.environ
+APP_NAME    = os.environ.get('APP_NAME') if ON_HEROKU else None
+BIND_ADDRESS= getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0')
 
+# Build FQDN & URL
+if not ON_HEROKU or getenv('FQDN'):
+    FQDN = getenv('FQDN', BIND_ADDRESS)
 else:
-    ON_HEROKU = False
-HAS_SSL=bool(getenv('HAS_SSL',True))
+    FQDN = f"{APP_NAME}.herokuapp.com"
+
+if ON_HEROKU or NO_PORT:
+    URL = f"https://{FQDN}/"
+else:
+    URL = f"https://{FQDN}:{PORT}/"
+
+SLEEP_THRESHOLD = int(os.environ.get('SLEEP_THRESHOLD', '60'))
+WORKERS         = int(os.environ.get('WORKERS', '4'))
+SESSION_NAME    = os.environ.get('SESSION_NAME', 'Kairnexbot')
+MULTI_CLIENT    = False
+name            = os.environ.get('name', 'kairnex')
+PING_INTERVAL   = int(getenv("PING_INTERVAL", "1200"))
+HAS_SSL         = is_enabled(getenv('HAS_SSL', 'True'), True)
+
 if HAS_SSL:
-    URL = "https://{}/".format(FQDN)
+    URL = f"https://{FQDN}/"
 else:
-    URL = "http://{}/".format(FQDN)
+    URL = f"http://{FQDN}/"
 
-# add premium logs channel id
-PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', ''))
+PREMIUM_LOGS = int(os.environ.get('PREMIUM_LOGS', '-1002083345153'))
 
-LOG_STR = "Current Cusomized Configurations are:-\n"
-LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
-LOG_STR += ("P_TTI_SHOW_OFF found , Users will be redirected to send /start to Bot PM instead of sending file file directly\n" if P_TTI_SHOW_OFF else "P_TTI_SHOW_OFF is disabled files will be send in PM, instead of sending start.\n")
-LOG_STR += ("SINGLE_BUTTON is Found, filename and files size will be shown in a single button instead of two separate buttons\n" if SINGLE_BUTTON else "SINGLE_BUTTON is disabled , filename and file_sixe will be shown as different buttons\n")
-LOG_STR += (f"CUSTOM_FILE_CAPTION enabled with value {CUSTOM_FILE_CAPTION}, your files will be send along with this customized caption.\n" if CUSTOM_FILE_CAPTION else "No CUSTOM_FILE_CAPTION Found, Default captions of file will be used.\n")
-LOG_STR += ("Long IMDB storyline enabled." if LONG_IMDB_DESCRIPTION else "LONG_IMDB_DESCRIPTION is disabled , Plot will be shorter.\n")
-LOG_STR += ("Spell Check Mode Is Enabled, bot will be suggesting related movies if movie not found\n" if SPELL_CHECK_REPLY else "SPELL_CHECK_REPLY Mode disabled\n")
-LOG_STR += (f"MAX_LIST_ELM Found, long list will be shortened to first {MAX_LIST_ELM} elements\n" if MAX_LIST_ELM else "Full List of casts and crew will be shown in imdb template, restrict them by adding a value to MAX_LIST_ELM\n")
-LOG_STR += f"Your current IMDB template is {IMDB_TEMPLATE}"
+# Final log string
+LOG_STR  = "Current Customized Configurations are:-\n"
+LOG_STR += ("IMDB Results are enabled; the bot will show IMDB details for your queries.\n" if IMDB else "IMDB Results are disabled.\n")
+LOG_STR += ("P_TTI_SHOW_OFF is enabled; users will be redirected to send /start to the bot's PM instead of sending files directly.\n" if P_TTI_SHOW_OFF else "P_TTI_SHOW_OFF is disabled; files will be sent in PM without redirection.\n")
+LOG_STR += ("SINGLE_BUTTON is enabled; filename and file size will be shown in a single button.\n" if SINGLE_BUTTON else "SINGLE_BUTTON is disabled; filename and file size will be shown as separate buttons.\n")
+LOG_STR += (f"CUSTOM_FILE_CAPTION is enabled with value: {CUSTOM_FILE_CAPTION}\n" if CUSTOM_FILE_CAPTION else "No CUSTOM_FILE_CAPTION found; default captions will be used.\n")
+LOG_STR += ("Long IMDB storyline is enabled.\n" if LONG_IMDB_DESCRIPTION else "Long IMDB storyline is disabled.\n")
+LOG_STR += ("Spell Check mode is enabled; the bot will suggest related movies if none are found.\n" if SPELL_CHECK_REPLY else "Spell Check mode is disabled.\n")
+LOG_STR += (f"MAX_LIST_ELM is set; lists will be shortened to the first {MAX_LIST_ELM} elements.\n" if MAX_LIST_ELM else "Full cast and crew lists will be shown; set MAX_LIST_ELM to limit.\n")
+LOG_STR += f"Your current IMDB template is: {IMDB_TEMPLATE}\n"
