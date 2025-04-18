62428dcca853489a0b25d45b463fcd025132c98b
Kairnex <codexkairnex@gmail.com>
2025-04-19 00:54:14 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -109,45 +109,54 @@ async def start(client, message):
             )
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
-         buttons = [[
-                    InlineKeyboardButton('Add To Group', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
-                ],[
-                    InlineKeyboardButton('Updates', callback_data='channels')
-                ],[
-                    InlineKeyboardButton('Commands', callback_data='help'),
-                    InlineKeyboardButton('About', callback_data='about')
-                ]]
-        
-        reply_markup = InlineKeyboardMarkup(buttons)
-        current_time = datetime.now(pytz.timezone(TIMEZONE))
-        curr_time = current_time.hour        
-        if curr_time < 12:
-            gtxt = "ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ 👋" 
-        elif curr_time < 17:
-            gtxt = "ɢᴏᴏᴅ ᴀғᴛᴇʀɴᴏᴏɴ 👋" 
-        elif curr_time < 21:
-            gtxt = "ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ 👋"
-        else:
-            gtxt = "ɢᴏᴏᴅ ɴɪɢʜᴛ 👋"
-        m=await message.reply_text("<i>ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ <b>ᴛʜᴇ ᴍᴏᴠɪᴇ ᴘʀᴏᴠɪᴅᴇʀ ʙᴏᴛ</b>.\nʜᴏᴘᴇ ʏᴏᴜ'ʀᴇ ᴅᴏɪɴɢ ᴡᴇʟʟ...</i>")
-        await asyncio.sleep(0.4)
-        await m.edit_text("👀")
-        await asyncio.sleep(0.5)
-        await m.edit_text("⚡")
-        await asyncio.sleep(0.5)
-        await m.edit_text("<b><i>ꜱᴛᴀʀᴛɪɴɢ...</i></b>")
-        await asyncio.sleep(0.4)
-        await m.delete()        
-        m=await message.reply_sticker("CAACAgQAAxkBAAEKeqNlIpmeUoOEsEWOWEiPxPi3hH5q-QACbg8AAuHqsVDaMQeY6CcRojAE") 
-        await asyncio.sleep(1)
-        await m.delete()
-        await message.reply_photo(
-            photo=random.choice(PICS),
-            caption=script.START_TXT.format(message.from_user.mention, gtxt, temp.U_NAME, temp.B_NAME),
-            reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
-        )
-        return
+       buttons = [[
+    InlineKeyboardButton('Add To Group', url=f'http://telegram.me/{temp.U_NAME}?startgroup=true')
+], [
+    InlineKeyboardButton('Updates', callback_data='channels')
+], [
+    InlineKeyboardButton('Commands', callback_data='help'),
+    InlineKeyboardButton('About', callback_data='about')
+]]
+
+reply_markup = InlineKeyboardMarkup(buttons)
+
+current_time = datetime.now(pytz.timezone(TIMEZONE))
+curr_time = current_time.hour
+
+if curr_time < 12:
+    gtxt = "ɢᴏᴏᴅ ᴍᴏʀɴɪɴɢ 👋" 
+elif curr_time < 17:
+    gtxt = "ɢᴏᴏᴅ ᴀғᴛᴇʀɴᴏᴏɴ 👋" 
+elif curr_time < 21:
+    gtxt = "ɢᴏᴏᴅ ᴇᴠᴇɴɪɴɢ 👋"
+else:
+    gtxt = "ɢᴏᴏᴅ ɴɪɢʜᴛ 👋"
+
+m = await message.reply_text(
+    "<i>ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ <b>ᴛʜᴇ ᴍᴏᴠɪᴇ ᴘʀᴏᴠɪᴅᴇʀ ʙᴏᴛ</b>.\nʜᴏᴘᴇ ʏᴏᴜ'ʀᴇ ᴅᴏɪɴɢ ᴡᴇʟʟ...</i>"
+)
+await asyncio.sleep(0.4)
+await m.edit_text("👀")
+await asyncio.sleep(0.5)
+await m.edit_text("⚡")
+await asyncio.sleep(0.5)
+await m.edit_text("<b><i>ꜱᴛᴀʀᴛɪɴɢ...</i></b>")
+await asyncio.sleep(0.4)
+await m.delete()
+
+m = await message.reply_sticker("CAACAgQAAxkBAAEKeqNlIpmeUoOEsEWOWEiPxPi3hH5q-QACbg8AAuHqsVDaMQeY6CcRojAE") 
+await asyncio.sleep(1)
+await m.delete()
+
+await message.reply_photo(
+    photo=random.choice(PICS),
+    caption=script.START_TXT.format(message.from_user.mention, gtxt, temp.U_NAME, temp.B_NAME),
+    reply_markup=reply_markup,
+    parse_mode=enums.ParseMode.HTML
+)
+
+return
+
         
         
     if len(message.command) == 2 and message.command[1] in [""]:
